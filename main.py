95e3ac38b5db0df023e7a2b0414cdda4f28a1159
P.J. Finlay <pjappdevelopment@gmail.com>
2022-10-16 15:51:51 -0500

    Made Jsonix class

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import pathlib
 import json
 
-db = pathlib.Path.home() / "JSONIX-Example.jsonl"
+
+class Jsonix:
+    def __init__(self, db):
+        self.db = db
+
+    def persist(self, o):
+        json_str = json.dumps(o)
+        with open(self.db, "a") as dbf:
+            dbf.write(json_str)
+            dbf.write("\n")
+
+    def query(self, q):
+        objects = list()
+        with open(self.db, "r") as dbf:
+            for line in dbf.readlines():
+                o = json.loads(line)
+                if q(o):
+                    objects.append(o)
+        return objects
 
 
-def persist(o):
-    json_str = json.dumps(o)
-    with open(db, "a") as dbf:
-        dbf.write(json_str)
-        dbf.write("\n")
+db = Jsonix(pathlib.Path.home() / "JSONIX-Example.jsonl")
 
 
-def query(q):
-    objects = list()
-    with open(db, "r") as dbf:
-        for line in dbf.readlines():
-            o = json.loads(line)
-            if q(o):
-                objects.append(o)
-    return objects
+db.persist({"name": "P.J. Finlay", "age": 24})
 
-
-persist({"name": "P.J. Finlay", "age": 24})
-
-print(query(lambda x: x["age"] == 24))
+print(db.query(lambda x: x["age"] == 24))
 # [{'name': 'P.J. Finlay', 'age': 24}]
